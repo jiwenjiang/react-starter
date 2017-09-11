e831f79b5a4ef4031c7b5edb2186d4c489140097
ji.wenjiang <513755565@qq.com>
2017-09-11 17:49:43 +0800

    pull codes

--- src/containers/home/detail/index.jsx
+++ src/containers/home/detail/index.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react'; // 引入了React和PropTypes
 import {Row, Col} from 'antd';
 import url from '../../../config/ip/image';
 import xhr from '../../../services/xhr/index';
+import './index.less';
 /* 以类的方式创建一个组件 */
 class Main extends Component {
     constructor(props) {
@@ -9,7 +10,7 @@ class Main extends Component {
     }
 
     componentDidMount() {
-        this.getData();
+        // this.getData();
     }
 
     getData() {
@@ -25,8 +26,8 @@ class Main extends Component {
         return (
             <div className="mg-top20">
                 <Row>
-                    <Col span={5}>
-                        <div style={{border: '1px solid red', height: '835px'}}>
+                    <Col span={5} className="img-detail">
+                        <div className="part">
                             <i className="iconfont icon-HEAD"></i>
                         </div>
                     </Col>
