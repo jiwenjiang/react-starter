6e0e454de11b0264e62a1a0b0f3e139cd826ad6e
ji.wenjiang <513755565@qq.com>
2017-09-12 17:59:50 +0800

    add pagenation for project

---
+++ src/containers/home/detail/seriesTB/index.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'; // 引入了React和PropTypes
+import './index.less';
+// import {Row, Col,Tabs } from 'antd';
+
+
+
+/* 以类的方式创建一个组件 */
+export class SeriesTB extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+
+    render() {
+        return (
+            <div className="mg-top20">
+               <table>
+                   <thead>
+                   <tr>
+
+                   </tr>
+                   </thead>
+               </table>
+            </div>
+        );
+    }
+}
+
